0875fbcc2e630905f90d73d87c1c373a2419571a
Isabely Gomes <80432122+issabely@users.noreply.github.com>
2025-04-21 17:16:13 -0300

    Decrementando o contador

--- app/index.jsx
+++ app/index.jsx
@@ -28,23 +28,46 @@ const pomodoro = [
 export default function Index() {
 
   const [timerType, setTimerType] = useState(pomodoro[0]);
-    
+  const [seconds, setSeconds] = useState(pomodoro[0].initialValue)
+  const [timerRunning, setTimerRunnig] = useState(false)
+ 
   const timerRef = useRef(null);
-    
+
+ const clear = () => {
+  if (timerRef.current != null) {
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+    setTimerRunnig(false);
+  }
+ }
+  const toggleTimerType = (newTimerType) => {
+    setTimerType(newTimerType)
+    setSeconds(newTimerType.initialValue)
+    clear()
+  }
   const toggleTimer = () => {
     if (timerRef.current) {
-      // pausar
-      clearInterval(timerRef.current);
+      clear()
       return;
     }
+
+    setTimerRunnig(true)
+
     const id = setInterval(() => {
+      setSeconds(oldeState => {
+        if(oldeState ===0){
+          clear()
+          return timerType.initialValue
+        }
+        return oldeState -1
+      })
       console.log('timer rolando');
     }, 1000);
-    timerRef.current = id;
+     timerRef.current = id;
   }
 
 
-  return (
+  return ( 
     <View
       style={styles.container}
     >
@@ -55,14 +78,14 @@ export default function Index() {
             <ActionButton
                key={p.id}
                active={ timerType.id === p.id }
-               onPress={() => setTimerType(p)}
+               onPress={() => toggleTimerType(p)}
                display={p.display}
             />
           ))}
         </View>
-       <Timer totalSeconds={timerType.initialValue}/>
+       <Timer totalSeconds={seconds}/>
        <FokusButton
-          title={timerRef.current ? 'Pausar' : 'Começar'}
+          title={timerRunning ? 'Pausar' : 'Começar'}
           onPress={toggleTimer}
         />
       </View>
